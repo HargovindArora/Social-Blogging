17ce36924dc4f893ee35c9f044608a01eaf36b76
HargovindArora <hargovinds50@gmail.com>
2017-09-21 01:32:09 +0530

    Accounts app completed

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ class UserCreateForm(UserCreationForm):
 
     class Meta:
         fields = ('username','email','password1','password2')
-        model = get_user_model
+        model = get_user_model()
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,6 @@ from . import forms
 # Create your views here.
 
 class SignUp(CreateView):
-    form_class = from.UserCreateForm
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
